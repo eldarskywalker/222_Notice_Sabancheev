4c3672394f1b10c6ed83b0e5cf051b21148c81fd
eldarskywalker <91009658+eldarskywalker@users.noreply.github.com>
2025-12-15 10:59:25 +0300

    Add files via upload
    add code

---
+++ UserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _222_Notice_Sabancheev
+{
+    public static class UserService
+    {
+        public static int UserID;
+    }
+}
